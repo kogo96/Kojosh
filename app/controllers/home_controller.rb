2e330370da6bed4b13ab3bd496c78d152cceb395
Kojonazar <kogo210696@gmail.com>
2014-12-06 09:44:29 +0400

    Мисал, проект

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def eric
+  end
+
+  def sasha
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kojosh::Application.config.secret_token = '8d0c99704b20ddaf21a267d16da7465bdda95cd3de8676b1313f0a351fc189f4f6b1f7801ef714cd0752d8ad1b8837e4d12a7a191a269e7747932becae1db455'
